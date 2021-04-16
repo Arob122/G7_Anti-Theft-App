b3589515ef6628b5ee46961d5dc91d8715c00a39
a-alsultan <arob2604@gmail.com>
2021-04-17 00:58:13 +0300

    بإذن الله انتهينا

--- app/src/main/java/com/example/g7anti_theftapp/HomeActivity.java
+++ app/src/main/java/com/example/g7anti_theftapp/HomeActivity.java
@@ -3,14 +3,38 @@ package com.example.g7anti_theftapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button signup, signin;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.first);
+
+        signup = (Button) findViewById(R.id.reg);
+        signin = (Button) findViewById(R.id.log);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                startActivity(intent);
+            }
+        });
+
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
